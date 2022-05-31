19ac5185aef6c5c30bd9362c7ac2ad6a5f9d4474
candradwicahyo <candradwicahyo18@gmail.com>
2022-06-01 00:14:32 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+## blob maker
+
+sebuah program dengan nama blob maker yang dibuat dengan bahasa pemrograman javascript
+
+### dihuat dengan berikut
+
+* HTML
+* CSS
+  * [Bootstrap](https://getbootstrap.com)
+* Javascript
+
+### lainnya
+
+* [SweetAlert2](https://sweetalert2.github.io)
+* handphone
+* code editor (Acode)
+* edit gambar (Pixellab)
+
+### preview
+
+![result](https://github.com/candradwicahyo/blob-maker/blob/master/preview.jpg)
+
+[tekan link ini untuk mencoba program ini](https://candradwicahyo.github.io/blob-maker)
+
+> dibuat oleh **candra dwi cahyo**
---
+++ script.js
@@ -0,0 +1,41 @@
+window.onload = () => {
+  
+  const item = document.querySelector('.item');
+  const input = document.querySelector('.input');
+  const inputRange = document.querySelectorAll('.input-range');
+  inputRange.forEach(input => input.addEventListener('input', setValue));
+  
+  function setValue() {
+    const range1 = document.querySelector('#range1').value;
+    const range2 = document.querySelector('#range2').value;
+    const range3 = document.querySelector('#range3').value;
+    const range4 = document.querySelector('#range4').value;
+    
+    item.style.borderRadius = putValue(range1, range2, range3, range4);
+    input.value = putValue(range1, range2, range3, range4, true);
+  }
+  
+  function putValue(range1, range2, range3, range4, show = false) {
+    return `${(show == true) ? 'border-radius:' : ''} ${range1}% ${100 - range1}% ${100 - range2}% ${range2}% / ${100 - range3}% ${range3}% ${range4}% ${100 - range4}%`;
+  }
+  
+  function sweetalert(text) {
+    return swal.fire ({
+      icon: 'success',
+      title: 'sucvess',
+      text: text
+    });
+  }
+  
+  function copy() {
+    const copyButton = document.querySelector('.copy');
+    copyButton.addEventListener('click', () => {
+      input.select();
+      document.execCommand("copy");
+      sweetalert('code has been copied!');
+    })
+  }
+  
+  copy();
+  
+}
